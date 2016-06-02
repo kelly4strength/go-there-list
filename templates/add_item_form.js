ad02166a1fde0187dcf1fc3281f7ac5c2c0eb526
Kelly Hoffer <kellyhoffer@Kellys-MacBook-Pro.local>
2016-06-01 18:05:40 -0700

    got the bootstrap and js set up and add items page is showing up

---
+++ templates/add_item_form.js
@@ -0,0 +1,43 @@
+"use strict";
+// the above is setting the max number of item forms that I want to pop up
+// the whole function is wrapped around this
+$(document).ready(function() {
+    console.log("setting fields")
+    var max_fields = 7;
+    var field = 1;
+
+    // this funtion in calling the entire form class
+    $("#itemForm")
+        .on("submit", function(e){ 
+            // .trim method trims out whitespace before and after string
+            if ($(".form-control").val()) === "") {
+                alert("Please fill out the entire form.");
+                e.preventDefault();
+            }
+        })
+        .on("click", ".addButton", function(e) {console.log("plus button clicked")
+            if (field < max_fields) {
+                $("#itemTemplate").clone()
+                                 .removeAttr("hidden")
+                                 .removeAttr("id")
+                                 .insertBefore("#itemTemplate");
+                field++;
+            }
+        })
+        .on("click", ".removeButton", function(e) {
+            if (field > 1){
+                $(this).parents(".form-group").remove();  
+                // Use the .parents() method to traverse up through the ancestors of the DOM tree
+                field--;
+            }
+        });
+        
+});
+
+
+
+
+
+
+
+
